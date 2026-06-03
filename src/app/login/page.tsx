ab6954c7c99ddc640446c666ef1a54ae023b7312
Marco Sabatini <marsab04188@stud.noroff.no>
2026-06-03 07:43:34 +0200

    creating login page, updating .env to .env.local, updating supabase link

---
+++ src/app/login/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+
+  async function signIn() {
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+    });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("Check your email!");
+    }
+  }
+
+  return (
+    <main className="p-8">
+        <nav>
+            <button className="bg-red-500 px-2 py-1 cursor-pointer text-white rounded-md "
+            onClick={() => supabase.auth.signOut()}
+            >
+            Logout
+        </button>
+        </nav>
+      <h1 className="text-2xl font-bold mb-4">
+        HR Login
+      </h1>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="border p-2 mr-2"
+      />
+
+      <button
+        onClick={signIn}
+        className="bg-black text-white px-4 py-2 cursor-pointer rounded-md"
+      >
+        Login
+      </button>
+    </main>
+  );
+}
